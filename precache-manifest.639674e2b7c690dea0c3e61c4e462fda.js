a838b4eb8b3558b44f82aa24349614d03986d26f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-03 16:06:14 +0000

    Deploy godban/browsers-support-badges to github.com/godban/browsers-support-badges.git:gh-pages

---
+++ precache-manifest.639674e2b7c690dea0c3e61c4e462fda.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb01363fcb4a3fc8779560731445b2a",
+    "url": "/browsers-support-badges/index.html"
+  },
+  {
+    "revision": "f4fdf545289bf2dfff14",
+    "url": "/browsers-support-badges/static/css/2.02d6bbd9.chunk.css"
+  },
+  {
+    "revision": "f672ce4934cf79b22633",
+    "url": "/browsers-support-badges/static/css/main.76fe0199.chunk.css"
+  },
+  {
+    "revision": "f4fdf545289bf2dfff14",
+    "url": "/browsers-support-badges/static/js/2.342227ee.chunk.js"
+  },
+  {
+    "revision": "f672ce4934cf79b22633",
+    "url": "/browsers-support-badges/static/js/main.b22a84af.chunk.js"
+  },
+  {
+    "revision": "5f16679959f5f0a414a1",
+    "url": "/browsers-support-badges/static/js/runtime-main.7f07380c.js"
+  },
+  {
+    "revision": "54693c71220367de907552710b4dc2bd",
+    "url": "/browsers-support-badges/static/media/book.54693c71.svg"
+  },
+  {
+    "revision": "1899110c1bc0f5f0037039003e166720",
+    "url": "/browsers-support-badges/static/media/check.1899110c.svg"
+  },
+  {
+    "revision": "a78a81f8d29d860f8d4bf7949d02b69b",
+    "url": "/browsers-support-badges/static/media/chevron-down.a78a81f8.svg"
+  },
+  {
+    "revision": "35a09128707e840662743c4f4e25d898",
+    "url": "/browsers-support-badges/static/media/chevron-up.35a09128.svg"
+  },
+  {
+    "revision": "60cabd4b202d46fe182b7ccee0f609a6",
+    "url": "/browsers-support-badges/static/media/clippy.60cabd4b.svg"
+  },
+  {
+    "revision": "207687a1a5ee00eb276adf2eebf83d5e",
+    "url": "/browsers-support-badges/static/media/mark-github.207687a1.svg"
+  },
+  {
+    "revision": "a0f8f5984e19d467ac8207a1e9fa26fb",
+    "url": "/browsers-support-badges/static/media/x.a0f8f598.svg"
+  }
+]);
